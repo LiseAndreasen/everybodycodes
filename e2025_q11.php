adac45f029c768b9f025ed82f2ee6dcc11cec6c2
LiseAndreasen <lise.andreasen.1967@gmail.com>
2025-11-21 04:06:50 +0100

    add visuals for rounds

--- e2025_q11.php
+++ e2025_q11.php
@@ -3,6 +3,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // constants
 
+$test = 0;
+
 ///////////////////////////////////////////////////////////////////////////
 // functions
 
@@ -27,8 +29,49 @@ function print_ducks($data, $rounds_used) {
 	printf("\nCheck sum: %6d\n\n", $check_sum);
 }
 
+// https://stackoverflow.com/questions/34034730/how-to-enable-color-for-php-cli
+
+// example
+// output red text
+// php -r 'echo "\033[31m some colored text \033[0m some white text \n";'
+//Examples:
+//formatPrint(['blue', 'bold', 'italic','strikethrough'], "Wohoo");
+
+function formatPrint(array $format=[],string $text = '') {
+	$codes=[
+		'bold'=>1,
+		'italic'=>3, 'underline'=>4, 'strikethrough'=>9,
+		'black'=>30,   'red'=>31,   'green'=>32,   'yellow'=>33,   
+		'blue'=>34,   'magenta'=>35,   'cyan'=>36,   'white'=>37,
+		'blackbg'=>40, 'redbg'=>41, 'greenbg'=>42, 'yellowbg'=>43, 
+		'bluebg'=>44, 'magentabg'=>45, 'cyanbg'=>46, 'lightgreybg'=>47
+	];
+	$formatMap = array_map(function ($v) use ($codes) {
+		return $codes[$v]; 
+		}, $format);
+	return "\e[".implode(';',$formatMap).'m'.$text."\e[0m";
+}
+
+function print_ducks_change($old_data, $data) {
+	$ducks = "";
+	foreach($old_data as $key => $col) {
+		$duck = sprintf("%3d ", $data[$key]);
+		if($old_data[$key] != $data[$key]) {
+			if($old_data[$key] < $data[$key]) {
+				$ducks .= formatPrint(['greenbg'], $duck);
+			} else {
+				$ducks .= formatPrint(['redbg'], $duck);
+			}
+		} else {
+			$ducks .= $duck;
+		}
+	}
+	print("$ducks\n");
+}
+
 function move_ducks($dir, $max, $rounds, $rounds_used) {
-	global $data;
+	global $data, $test;
+	$old_data = $data;
 	$columns = sizeof($data);
 	$ducks_moved = 1;
 	while($ducks_moved == 1 && ($rounds_used < $rounds || $max == "n")) {
@@ -48,6 +91,10 @@ function move_ducks($dir, $max, $rounds, $rounds_used) {
 		if($ducks_moved == 1) {
 			$rounds_used++;
 		}
+		if($dir == "l" && $test == 1) {
+			print_ducks_change($old_data, $data);
+		}
+		$old_data = $data;
 	}
 	return $rounds_used;
 }
@@ -84,6 +131,7 @@ $rounds_used = 0;
 
 // first move right
 $rounds_used = move_ducks("r", "y", $rounds, $rounds_used);
+print_ducks_change($data, $data);
 
 // then move left
 $rounds_used = move_ducks("l", "y", $rounds, $rounds_used);
@@ -93,7 +141,7 @@ print_ducks($data, $rounds_used);
 ///////////////////////////////////////////////////////////////////////////
 // main program, part 2
 
-$file2 = './everybody_codes_e2025_q11_p2_ex2.txt';
+$file2 = './everybody_codes_e2025_q11_p2.txt';
 $input = file_get_contents($file2, true);
 $data = get_input($input);
 $columns = sizeof($data);
@@ -102,6 +150,7 @@ $rounds_used = 0;
 
 // first move right
 $rounds_used = move_ducks("r", "n", 0, $rounds_used);
+print_ducks_change($data, $data);
 
 // then move left
 $rounds_used = move_ducks("l", "n", 0, $rounds_used);
