75c06c8f210089401b0c8cce204b1ef96b0d9ac7
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-08-26 00:34:06 +0200

    part 2 added

--- e4_q01.php
+++ e4_q01.php
@@ -20,39 +20,56 @@ function get_input($input) {
 	return $data;
 }
 
+function jump($data, $where_min, $part) {
+    $final_sum = 0;
+    
+    foreach($data as $line) {
+        $visited = [];
+        $where = 0;
+        $visited[$where] = $where;
+        foreach($line as $no) {
+            $new_where = $where - $no;
+            if($new_where < $where_min || isset($visited[$new_where])) {
+                $new_where = $where + $no;
+                if(2 == $part) {
+                    while(isset($visited[$new_where])) {
+                        $new_where++;
+                    }
+                }
+            }
+            $where = $new_where;
+            $visited[$where] = $where;
+        }
+        $final_sum += $where;
+    }
+    return $final_sum;
+}
+
 ///////////////////////////////////////////////////////////////////////////
 // main program, part 1
 
 $file1 = './everybody_codes_e4_q' . $quest . '_p1.txt';
 $input = file_get_contents($file1, true);
 $data = get_input($input);
-$where_min = 0;
-$final_sum = 0;
+$part = 1;
 
-foreach($data as $line) {
-    $visited = [];
-    $where = 0;
-    $visited[$where] = $where;
-    foreach($line as $no) {
-        $new_where = $where - $no;
-        if($new_where < $where_min || isset($visited[$new_where])) {
-            $new_where = $where + $no;
-        }
-        $where = $new_where;
-        $visited[$where] = $where;
-    }
-    $final_sum += $where;
-}
+$where_min = 0;
+$final_sum = jump($data, $where_min, $part); 
 
 printf("Result 1: %d\n", $final_sum);
 
 ///////////////////////////////////////////////////////////////////////////
 // main program, part 2
 
-$file2 = './everybody_codes_e4_q' . $quest . '_p2_ex1.txt';
-//$input = file_get_contents($file2, true);
-//$data = get_input($input);
-//printf("Result 2: %d\n", $hits);
+$file2 = './everybody_codes_e4_q' . $quest . '_p2.txt';
+$input = file_get_contents($file2, true);
+$data = get_input($input);
+$part = 2;
+
+$where_min = 0;
+$final_sum = jump($data, $where_min, $part);
+
+printf("Result 2: %d\n", $final_sum);
 
 ///////////////////////////////////////////////////////////////////////////
 // main program, part 3
