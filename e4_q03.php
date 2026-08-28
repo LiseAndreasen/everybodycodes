b7562faadd670427a9ef145894f70b952953bf8b
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-08-28 12:06:36 +0200

    part 3 added

--- e4_q03.php
+++ e4_q03.php
@@ -26,7 +26,20 @@ function get_input($input) {
 	return $data;
 }
 
+// change (y, x) map to (x, y) map
+function pivot($map) {
+    $map_height = sizeof($map);
+    $map_width = sizeof($map[0]);
+    for($j=0;$j<$map_height;$j++) {
+        for($i=0;$i<$map_width;$i++) {
+            $map2[$i][$j] = $map[$j][$i];
+        }
+    }
+    return $map2;
+}
+
 function print_map($map) {
+    $map = pivot($map);
     foreach($map[0] as $j => $cell) {
         foreach($map as $i => $col) {
             echo $map[$i][$j];
@@ -37,6 +50,29 @@ function print_map($map) {
         echo "=";
     }
     echo "\n";
+}
+
+// https://stackoverflow.com/questions/34034730/how-to-enable-color-for-php-cli
+
+// example
+// output red text
+// php -r 'echo "\033[31m some colored text \033[0m some white text \n";'
+//Examples:
+//formatPrint(['blue', 'bold', 'italic','strikethrough'], "Wohoo");
+
+function formatPrint(array $format=[],string $text = '') {
+    $codes=[
+        'bold'=>1,
+        'italic'=>3, 'underline'=>4, 'strikethrough'=>9,
+        'black'=>30,   'red'=>31,   'green'=>32,   'yellow'=>33,
+        'blue'=>34,   'magenta'=>35,   'cyan'=>36,   'white'=>37,
+        'blackbg'=>40, 'redbg'=>41, 'greenbg'=>42, 'yellowbg'=>43,
+        'bluebg'=>44, 'magentabg'=>45, 'cyanbg'=>46, 'lightgreybg'=>47
+    ];
+    $formatMap = array_map(function ($v) use ($codes) {
+        return $codes[$v];
+    }, $format);
+        return "\e[".implode(';',$formatMap).'m'.$text."\e[0m";
 }
 
 function construct_up_and_left($width, $height, $horizontal_offsets,
@@ -130,7 +166,7 @@ function count_surrounded2($height, $width, $up, $left) {
         }
     }
     
-    return max($surrounded);
+    return $surrounded;
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -177,16 +213,197 @@ $vertical_offsets_size = sizeof($vertical_offsets);
 [$up, $left] = construct_up_and_left($width, $height, $horizontal_offsets,
     $horizontal_offsets_size, $vertical_offsets, $vertical_offsets_size);
 
-$largest_surrounded = count_surrounded2($height, $width, $up, $left);
+$largest_surrounded = max(count_surrounded2($height, $width, $up, $left));
 
 printf("Result 2: %d\n", $largest_surrounded);
 
 ///////////////////////////////////////////////////////////////////////////
 // main program, part 3
 
-$file3 = './everybody_codes_e4_q' . $quest . '_p3_ex1.txt';
-//$input = file_get_contents($file3, true);
-//$data = get_input($input);
-//printf("Result 3: %d\n", $hits);
+$file3 = './everybody_codes_e4_q' . $quest . '_p3.txt';
+$input = file_get_contents($file3, true);
+$data = get_input($input);
+$width = $data["width"];
+$height = $data["height"];
+$horizontal_offsets = $data["horizontal-offsets"];
+$horizontal_offsets_size = sizeof($horizontal_offsets);
+$vertical_offsets = $data["vertical-offsets"];
+$vertical_offsets_size = sizeof($vertical_offsets);
+
+// construct up and left for a slice of the floor
+// with width and height double that of the repeating offset sizes
+// this slice will repeat
+
+$slice_width = $vertical_offsets_size * 2;
+$slice_height = $horizontal_offsets_size * 2;
+
+[$slice_up, $slice_left] = construct_up_and_left($slice_width,
+    $slice_height, $horizontal_offsets, $horizontal_offsets_size,
+    $vertical_offsets, $vertical_offsets_size);
+
+$slice_surrounded = count_surrounded2($slice_height, $slice_width,
+    $slice_up, $slice_left);
+
+///////////////////////////////////////////////////////////////////////////
+
+$slice_width_string_8 = formatPrint(['redbg'],
+    sprintf("%8s", $slice_width));
+$slice_width_string_16 = formatPrint(['redbg'],
+    sprintf("%16s", $slice_width));
+
+$slice_height_string_8 = formatPrint(['greenbg'],
+    sprintf("%8s", $slice_height));
+$slice_height_string_16 = formatPrint(['greenbg'],
+    sprintf("%16s", $slice_height));
+
+printf("\n%s %36s is width of slice\n", $slice_width_string_16, "");
+printf("%s %36s is height of slice\n", $slice_height_string_16, "");
+
+$slice_width_no_string_12 = formatPrint(['bluebg'],
+    sprintf("%12s", floor($width / $slice_width)));
+
+$slice_height_no_string_8 = formatPrint(['yellowbg'],
+    sprintf("%8s", floor($height / $slice_height)));
+$slice_height_no_string_12 = formatPrint(['yellowbg'],
+    sprintf("%12s", floor($height / $slice_height)));
+
+$right_edge_width = $width % $slice_width;
+$down_edge_height = $height % $slice_height;
+
+$right_edge_width_string_8 = formatPrint(['magentabg'],
+    sprintf("%8s", $right_edge_width));
+$right_edge_width_string_16 = formatPrint(['magentabg'],
+    sprintf("%16s", $right_edge_width));
+
+$down_edge_height_string_8 = formatPrint(['cyanbg'],
+    sprintf("%8s", $down_edge_height));
+$down_edge_height_string_16 = formatPrint(['cyanbg'],
+    sprintf("%16s", $down_edge_height));
+
+printf("%16d = %s * %s + %s is width of floor\n", $width,
+    $slice_width_no_string_12, $slice_width_string_8,
+    $right_edge_width_string_8);
+printf("%16d = %s * %s + %s is height of floor\n", $height,
+    $slice_height_no_string_12, $slice_height_string_8,
+    $down_edge_height_string_8);
+
+$repeating_slices = floor($width / $slice_width)
+    * floor($height / $slice_height);
+
+$repeating_slices_string_12 = formatPrint(['lightgreybg', 'black'],
+    sprintf("%12s", $repeating_slices));
+$repeating_slices_string_16 = formatPrint(['lightgreybg', 'black'],
+    sprintf("%16s", $repeating_slices));
+    
+printf("%s = %s * %s %10s repeating slices covers most of the floor\n",
+    $repeating_slices_string_16, $slice_width_no_string_12,
+    $slice_height_no_string_8, "");
+
+$repeating_slices_id = 1;
+
+$surrounded = [];
+
+$surrounded[0][$repeating_slices_id] = $repeating_slices
+    * $slice_surrounded[0];
+$surrounded[1][$repeating_slices_id] = $repeating_slices
+    * $slice_surrounded[1];
+
+printf("%16d = %s * %8d %10s surrounded tiles in the 0 group\n",
+    $surrounded[0][$repeating_slices_id], $repeating_slices_string_12,
+    $slice_surrounded[0], "");
+printf("%16d = %s * %8d %10s surrounded tiles in the 1 group\n",
+    $surrounded[1][$repeating_slices_id], $repeating_slices_string_12,
+    $slice_surrounded[1], "");
+
+///////////////////////////////////////////////////////////////////////////
+
+printf("\n%s %36s is width of remaining edge on the right\n",
+    $right_edge_width_string_16, "");
+
+[$slice_right_up, $slice_right_left] = construct_up_and_left($right_edge_width,
+    $slice_height, $horizontal_offsets, $horizontal_offsets_size,
+    $vertical_offsets, $vertical_offsets_size);
+
+$slice_right_surrounded = count_surrounded2($slice_height, $right_edge_width,
+    $slice_right_up, $slice_right_left,
+    $horizontal_offsets_size, $vertical_offsets_size);
+
+$right_edge_id = 2;
+
+$surrounded[0][$right_edge_id] = floor($height / $slice_height)
+    * $slice_right_surrounded[0];
+$surrounded[1][$right_edge_id] = floor($height / $slice_height)
+    * $slice_right_surrounded[1];
+
+printf("%16d = %s * %8d %10s surrounded tiles in the 0 group\n",
+    $surrounded[0][$right_edge_id], $slice_height_no_string_12,
+    $slice_right_surrounded[0], "");
+printf("%16d = %s * %8d %10s surrounded tiles in the 1 group\n",
+    $surrounded[1][$right_edge_id], $slice_height_no_string_12,
+    $slice_right_surrounded[1], "");
+
+///////////////////////////////////////////////////////////////////////////
+
+printf("\n%s %36s is height of remaining edge below\n",
+    $down_edge_height_string_16, "");
+
+[$slice_down_up, $slice_down_left] = construct_up_and_left($slice_width,
+    $down_edge_height, $horizontal_offsets, $horizontal_offsets_size,
+    $vertical_offsets, $vertical_offsets_size);
+
+$slice_down_surrounded = count_surrounded2($down_edge_height, $slice_width,
+    $slice_down_up, $slice_down_left,
+    $horizontal_offsets_size, $vertical_offsets_size);
+
+$down_edge_id = 3;
+
+$surrounded[0][$down_edge_id] = floor($width / $slice_width)
+    * $slice_down_surrounded[0];
+$surrounded[1][$down_edge_id] = floor($width / $slice_width)
+    * $slice_down_surrounded[1];
+
+printf("%16d = %s * %8d %10s surrounded tiles in the 0 group\n",
+    $surrounded[0][$down_edge_id], $slice_width_no_string_12,
+    $slice_down_surrounded[0], "");
+printf("%16d = %s * %8d %10s surrounded tiles in the 1 group\n",
+    $surrounded[1][$down_edge_id], $slice_width_no_string_12,
+    $slice_down_surrounded[1], "");
+
+///////////////////////////////////////////////////////////////////////////
+
+[$slice_corner_up, $slice_corner_left] = construct_up_and_left($right_edge_width,
+    $down_edge_height, $horizontal_offsets, $horizontal_offsets_size,
+    $vertical_offsets, $vertical_offsets_size);
+
+$slice_corner_surrounded = count_surrounded2($down_edge_height,
+    $right_edge_width, $slice_corner_up, $slice_corner_left,
+    $horizontal_offsets_size, $vertical_offsets_size);
+
+printf("\n%s %36s is width of remaining corner\n",
+    $right_edge_width_string_16, "");
+printf("%s %36s is height of remaining corner\n",
+    $down_edge_height_string_16, "");
+
+$corner_id = 4;
+
+$surrounded[0][$corner_id] = $slice_corner_surrounded[0];
+$surrounded[1][$corner_id] = $slice_corner_surrounded[1];
+
+printf("%16d = %12d * %8d %10s surrounded tiles in the 0 group\n",
+    $surrounded[0][$corner_id], 1, $slice_corner_surrounded[0], "");
+printf("%16d = %12d * %8d %10s surrounded tiles in the 1 group\n",
+    $surrounded[1][$corner_id], 1, $slice_corner_surrounded[1], "");
+
+
+///////////////////////////////////////////////////////////////////////////
+
+printf("\n%16d %36s surrounded tiles in the 0 group\n",
+    array_sum($surrounded[0]), "");
+printf("%16d %36s surrounded tiles in the 1 group\n",
+    array_sum($surrounded[1]), "");
+
+$result = max(array_sum($surrounded[0]), array_sum($surrounded[1]));
+
+printf("\nResult 3: %d\n", $result);
 
 ?>
